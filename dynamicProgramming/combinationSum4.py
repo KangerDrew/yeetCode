65ace615226c80f4aec56b137a49b174d8e25837
KangerDrew <kangdrew95@gmail.com>
2022-05-29 00:49:14 -0700

    Improved version using dictionary (hashmap) instead of list (array)

--- dynamicProgramming/combinationSum4.py
+++ dynamicProgramming/combinationSum4.py
@@ -13,5 +13,20 @@ def combinationSum4(nums, target):
     return combo_list[-1]
 
 
-print(combinationSum4([1, 2, 3], 4))
-print(combinationSum4([3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25], 10))
+# print(combinationSum4([1, 2, 3], 4))
+# print(combinationSum4([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], 10))
+
+
+def improved(nums, target):
+    dp = { 0 : 1}
+
+    for total in range(1, target + 1):
+        dp[total] = 0
+        for n in nums:
+            dp[total] += dp.get(total - n, 0)
+
+    return dp[target]
+
+
+print(improved([1, 2, 3], 4))
+print(improved([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], 10))
