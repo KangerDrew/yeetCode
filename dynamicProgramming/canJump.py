e330bfa181dbb7fab6427809fcd6219968b5291c
KangerDrew <kangdrew95@gmail.com>
2022-05-29 13:22:14 -0700

    Backtracking greedy solution. Requires review

--- dynamicProgramming/canJump.py
+++ dynamicProgramming/canJump.py
@@ -27,3 +27,27 @@ print(canJumpSlow([2, 3, 1, 1, 4]))
 print(canJumpSlow([3, 2, 1, 0, 4]))
 
 
+# Instead of a traditional dynamic programming approach,
+# we can use greedy approach to solve this problem:
+def canJumpGreedy(nums):
+    edge = 0
+
+    for i, num in enumerate(reversed(nums)):
+        if i - num <= edge:
+            edge = i
+
+    return edge == len(nums) - 1
+
+
+print(canJumpGreedy([2, 3, 1, 1, 4]))
+print(canJumpGreedy([3, 2, 1, 0, 4]))
+
+
+def canJumpGreedyAlt(nums):
+    goal = len(nums) - 1
+
+    for i in range(len(nums) - 1, -1, -1):
+        if i + nums[i] >= goal:
+            goal = i
+
+    return goal == 0
