a944f4fd92123b9be63ff5f887b7a2a21136da61
KangerDrew <kangdrew95@gmail.com>
2022-05-30 13:21:23 -0700

    Brushing up on DFS

---
+++ graph/theoryPractice/dfs.py
@@ -0,0 +1,42 @@
+def DFS(graph, source):
+
+    stack = [source]
+
+    while len(stack) > 0:
+        current = stack.pop()
+        print(current)
+
+        for neighbor in graph[current]:
+            stack.append(neighbor)
+
+    return None
+
+
+sample = {
+    "a": ["b", "c"],
+    "b": ["d"],
+    "c": ["e"],
+    "d": ["f"],
+    "e": [],
+    "f": [],
+}
+
+DFS(sample, "a")
+
+
+def DFSRecursive(graph, source):
+    print(source)
+
+    for item in graph[source]:
+        DFSRecursive(graph, item)
+
+    return None
+
+
+DFSRecursive(sample, "a")
+
+
+# Note how DFSRecursive traversed b first instead of c,
+# this is because regular DFS uses stack, where it appended
+# b first, which led to c being "popped off" earlier and
+# getting traversed before c!
