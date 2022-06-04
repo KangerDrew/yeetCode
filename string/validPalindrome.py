f88060ba17a8ad911b2edf72cf0bdabf8b86debe
KangerDrew <kangdrew95@gmail.com>
2022-06-03 19:54:56 -0700

    cool two-liner for validPalindrome

--- string/validPalindrome.py
+++ string/validPalindrome.py
@@ -1,8 +1,11 @@
+import re
+
+
 def validPalindrome(s):
 
     def validString(x):
-        return (ord("a") <= ord(x.lower()) and ord(x.lower()) <= ord("z")) or (
-                    ord("0") <= ord(x) and ord(x) <= ord("9"))
+        return (ord("a") <= ord(x.lower()) <= ord("z")) or (
+                ord("0") <= ord(x) <= ord("9"))
 
     left, right = 0, len(s) - 1
 
@@ -22,4 +25,17 @@ def validPalindrome(s):
     return True
 
 
-print(validPalindrome("A man, a plan, a canal: Panama"))
+# print(validPalindrome("A man, a plan, a canal: Panama"))
+print(validPalindrome(". ,"))
+
+
+def superShort(s):
+    # Use regex to remove any characters that do not match a-z, A-Z, or 0-9,
+    # then lowercase them:
+    s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+
+    # s[::-1] returns a flipped s. If it's a valid palindrome, string should be same:
+    return s == s[::-1]
+
+
+print(superShort("A man, a plan, a canal: Panama"))
