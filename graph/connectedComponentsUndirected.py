60f72cd9a99cf34b4b5ebdc560e28ba09bd23841
KangerDrew <kangdrew95@gmail.com>
2022-06-07 22:56:08 -0700

    find and union function defined, w descriptions

--- graph/connectedComponentsUndirected.py
+++ graph/connectedComponentsUndirected.py
@@ -56,4 +56,41 @@ def countComponentsUnion(n, edges):
     # node with the larger rank will become the new parent:
     rank = [1] * n
 
+    # find() function returns the "parent" of the given node:
+    def find(node):
 
+        # If current node's parent its own parent:
+        if node == parents[node]:
+            return node
+
+        # Otherwise, we must recursively track the
+        # parent of the node:
+        parents[node] = parents[parents[node]]
+        return find(parents[node])
+
+    # union() function will combine two group into one, and
+    # return 1 if union did occur. If no union can occur (i.e.
+    # two nodes have the same parent), then we return 0:
+    def union(node1, node2):
+        # Get the parents of node1 and node 2:
+        p1, p2 = find(node1), find(node2)
+
+        # If the nodes have the same parent, they already belong
+        # to the same group. Thus no union occurs, and we return 0:
+        if p1 == p2:
+            return 0
+
+        # Check which parent node has a higher rank. The parent with
+        # lower rank will take the larger ranking parent as its new parent!
+        # Furthermore, we will increase the rank of the larger parent by
+        # the rank of the smaller parent:
+        if rank[p2] > rank[p1]:
+            parents[p1] = p2
+            rank[p2] += rank[p1]
+        else:
+            parents[p2] = p1
+            rank[p1] += rank[p2]
+
+        # After union process is successful, we return 1, signifying that
+        # there are now 1 less group!
+        return 1
