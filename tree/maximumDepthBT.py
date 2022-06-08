cef2e69f0d89513ef551571b549556c0f90a2fea
KangerDrew <kangdrew95@gmail.com>
2022-06-08 02:17:14 -0700

    Iterative DFS solution added

--- tree/maximumDepthBT.py
+++ tree/maximumDepthBT.py
@@ -52,3 +52,40 @@ def maximumDepthItrBFS(root):
         depth += 1
 
     return depth
+
+
+# Iterative DFS solution. This works bit differently than the recursive DFS solution,
+# since we need some way to keep track of the depth of each node:
+def maximumDepthItrDFS(root):
+
+    if not root:
+        return 0
+
+    # As mentioned in the description, we need some way to keep track of the
+    # depth of each node. To do this, we'll just use a length 2 array (list)
+    # where array[0] is the node, and array[1] is the depth of the node:
+    stack = [[root, 1]]
+    # Define a variable to keep track of the max depth:
+    max_depth = 1
+
+    while stack:
+
+        # Remove (pop off the stack) the last element from stack:
+        node, current_depth = stack.pop()
+
+        # Below if statement only executes if node is not None.
+        # This is because, if node.left is None (null node),
+        # the "depth" value of the null node shouldn't be considered
+        # since it's invalid:
+        if node:
+            # Take the largest value between max_depth and current_depth:
+            max_depth = max(max_depth, current_depth)
+
+            # Append two child node of the current node. As mentioned,
+            # if either of the nodes are null, the if statement will
+            # not consider that depth value:
+            stack.append([node.left, current_depth + 1])
+            stack.append([node.right, current_depth + 1])
+
+    return max_depth
+
