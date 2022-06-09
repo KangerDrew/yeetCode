5e708db395f6db7be3a01aca9a7fc0cf718b1059
KangerDrew <kangdrew95@gmail.com>
2022-06-09 02:22:51 -0700

    Comments added

--- tree/levelOrderTraversal.py
+++ tree/levelOrderTraversal.py
@@ -7,19 +7,27 @@ def levelOrderTrav(root):
 
     # use python's deque:
     queue = deque([root])
+    # Keep track of the total level order:
     level_order = []
 
     while queue:
-
+        # We will be using BFS to traverse each level one by one:
         current_level = []
 
+        # With each iteration of while loop, we process all the
+        # content in the queue, since each while loop should populate
+        # queue with all the nodes in one level:
         for i in range(len(queue)):
             node = queue.popleft()
+            # Only process the node if it's not null:
             if node:
                 current_level.append(node.val)
+                # Append its children to the queue:
                 queue.append(node.left)
                 queue.append(node.right)
 
+        # We need this if statement, because the for loop above will
+        # also append (but not process) a null nodes below our final level:
         if current_level:
             level_order.append(current_level)
 
