d5ad7263ceb1557d9b734846aac780b12744af51
KangerDrew <kangdrew95@gmail.com>
2022-06-08 23:34:12 -0700

    Iterative solution added w description

--- tree/sameTree.py
+++ tree/sameTree.py
@@ -14,3 +14,36 @@ def isSameTree(p, q):
 
     return True
 
+
+# We can use either DFS or BFS. It's just a matter of
+# simultaneously traversing both nodes at the same time:
+def isSameTreeItr(p, q):
+
+    stack = [[p, q]]
+
+    while stack:
+
+        current_p, current_q = stack.pop()
+
+        # If we reach a leaf node without any conflict, skip to the
+        # next item on stack:
+        if not current_p and not current_q:
+            continue
+
+        # If only one of them is None, then we have a mismatch. Return False:
+        if (not current_p and current_q) or (not current_q and current_p):
+            return False
+
+        # If the values do not match, then we have a mismatch. Return False:
+        if current_p.val != current_q.val:
+            return False
+
+        # Add left and right node (even if they're none) of p and q, together:
+        if current_p or current_q:
+            stack.append([current_p.left, current_q.left])
+            stack.append([current_p.right, current_q.right])
+
+    # If while loop exited without returning False, it means all nodes matched,
+    # so we can return True:
+    return True
+
