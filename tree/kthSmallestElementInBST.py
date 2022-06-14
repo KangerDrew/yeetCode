a4072a7e51028036441a227ef20ff7c7255f5aae
KangerDrew <kangdrew95@gmail.com>
2022-06-14 00:58:40 -0700

    comments added for iterative solution

--- tree/kthSmallestElementInBST.py
+++ tree/kthSmallestElementInBST.py
@@ -32,28 +32,36 @@ def kthSmallest(root, k):
 
 def kthSmallestItr(root, k):
 
-    # create stack
+    # create stack, and current node pointer
     stack = []
     current = root
 
     while current or stack:
 
+        # The nested while loop below will only run when we are
+        # seeking the leftmost node from the current node:
         while current:
             stack.append(current)
             current = current.left
 
+        # Remove the most recent entry from stack:
         current = stack.pop()
+        # Decrement k value by 1:
         k -= 1
 
+        # If we reached k = 0, it means that the current.val
+        # is the kth smallest element:
         if k == 0:
             return current.val
 
+        # If above if statement return didn't trigger, it means
+        # we need to traverse deeper into right, or pop back to the parent node.
+
+        # 1) If former is true, setting current to current.right will do exactly that
+
+        # 2) If the latter is true, setting current to current.right (which should be null)
+        # will cause the nested while loop at the beginning to skip over, and have the
+        # main while loop execute current = stack.pop() immediately. The latest node in stack
+        # should be the parent of the current node!
         current = current.right
 
-
-two = TreeNode(2)
-one = TreeNode(1, None, two)
-four = TreeNode(4)
-three = TreeNode(3, one, four)
-
-print(kthSmallestItr(three, 1))
