d717839f66bf7b328304ccec602aa6be81b41793
KangerDrew <kangdrew95@gmail.com>
2022-06-14 00:39:14 -0700

    Iterative solution added, with sample

--- tree/kthSmallestElementInBST.py
+++ tree/kthSmallestElementInBST.py
@@ -1,3 +1,10 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 # My first attempt:
 # I re-cycled my first attempt from validateBST problem, where I create
 # inorder array, and instead of checking the order, return the kth smallest
@@ -21,3 +28,32 @@ def kthSmallest(root, k):
 
     # Return the kth smallest element (k - 1 index)
     return inorder[k - 1]
+
+
+def kthSmallestItr(root, k):
+
+    # create stack
+    stack = []
+    current = root
+
+    while current or stack:
+
+        while current:
+            stack.append(current)
+            current = current.left
+
+        current = stack.pop()
+        k -= 1
+
+        if k == 0:
+            return current.val
+
+        current = current.right
+
+
+two = TreeNode(2)
+one = TreeNode(1, None, two)
+four = TreeNode(4)
+three = TreeNode(3, one, four)
+
+print(kthSmallestItr(three, 1))
