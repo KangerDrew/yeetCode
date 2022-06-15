414b3f87b2ad904f3ca2533cfcaf2a9ead325546
KangerDrew <kangdrew95@gmail.com>
2022-06-14 19:42:13 -0700

    Recursive solution explanation added

--- trie/designAndAddSearchWords.py
+++ trie/designAndAddSearchWords.py
@@ -31,27 +31,41 @@ class WordDictionary:
     # to match any letter:
     def search(self, word):
 
-        def dfs_search(node, word_index):
+        # The dfs function below takes TrieNode, and the
+        #
+        def dfs_search_recursive(node, starting_word_index):
 
+            # Take the input node as current:
             current = node
 
-            for i in range(word_index, len(word)):
+            # Loop through each index of the word, starting from
+            # the starting_word_index:
+            for i in range(starting_word_index, len(word)):
+
                 letter = word[i]
-
+                # If the letter we're looping through is ".", it means we
+                # have to check every single entries made on this level from
+                # current node:
                 if letter == ".":
+                    # Loop through every entry from current node's children:
                     for entry in current.children:
-
-                        if dfs_search(current.children[entry], i + 1):
+                        # Recursively execute dfs_search_recursive, using
+                        # current.children[entry] TrieNode. Also use i + 1
+                        # as starting_word index, to correctly loop from the
+                        # proper starting position:
+                        if dfs_search_recursive(current.children[entry], i + 1):
                             return True
-
+                    # if above didn't return True, it means no search was returned:
                     return False
                 else:
+                    # Below is the exact same search function as that from
+                    # implementTrie problem:
                     if letter not in current.children:
                         return False
                     current = current.children[letter]
 
             return current.endOfWord
 
-        # Execute the dfs_search from root:
-        return dfs_search(self.root, 0)
+        # Execute the search from root using helper function:
+        return dfs_search_recursive(self.root, 0)
 
