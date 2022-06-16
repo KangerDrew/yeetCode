6cced41a45198ba48b841b1825ac344ba0595c98
KangerDrew <kangdrew95@gmail.com>
2022-06-15 21:16:24 -0700

    spiral matrix problem setup. Will come back later...

---
+++ matrix/spiralMatrix.py
@@ -0,0 +1,8 @@
+# The idea is to use multiple for loops for traveling in a
+# spiral motion around the matrix.
+
+# We will also define boundaries to determine where we'll be
+# starting our for loops from - where the boundary values will
+# increase/decrease as we travel in spiral around the matrix:
+def spiralOrder(matrix):
+    pass
