881bf28fdd2ba60b036b70815329b1139b931bb2
KangerDrew <kangdrew95@gmail.com>
2022-06-17 03:29:56 -0700

    indexing len=3 array issue solved. Now need to add explanation for divide and conquer approach

--- array/maxSubArrayDivideAndConquer.py
+++ array/maxSubArrayDivideAndConquer.py
@@ -3,7 +3,7 @@ def middleOutMax(nums, mid_index):
     current_sum = 0
     max_sum_left = float('-inf')
 
-    for i in range(mid_index, -1, -1):
+    for i in range(mid_index - 1, -1, -1):
         current_sum += nums[i]
         if current_sum > max_sum_left:
             max_sum_left = current_sum
@@ -14,7 +14,7 @@ def middleOutMax(nums, mid_index):
     current_sum = 0
     max_sum_right = float('-inf')
 
-    for j in range(mid_index + 1, len(nums)):
+    for j in range(mid_index, len(nums)):
         current_sum += nums[j]
         if current_sum > max_sum_right:
             max_sum_right = current_sum
@@ -25,7 +25,7 @@ def middleOutMax(nums, mid_index):
     return max_sum_left + max_sum_right
 
 
-#
+# Main function:
 def maxSubArrDivideAndConquer(nums):
 
     if len(nums) == 0:
@@ -39,7 +39,7 @@ def maxSubArrDivideAndConquer(nums):
     # Taking mid_index - 1 as a midpoint seems to address the
     # issue with len = 3 arrays (specifically [1, 2, -1]) that
     # makes this algorithm calculate incorrect sum...
-    mid_sum = middleOutMax(nums, mid_index - 1)
+    mid_sum = middleOutMax(nums, mid_index)
 
     return max(left_sum, right_sum, mid_sum)
 
@@ -62,6 +62,17 @@ def maxSubArrDivideAndConquer(nums):
 # having values, we'll get different result depending on where we take our
 # midpoint value (HOWEVER inputting mid_index -1 on helper function seems
 # to fix the issue...)
+
+# Edge case solved. It was the issue with the indexing on line 6 and line 17
+# of helper function (for loop range).
+#
+# Initial setup was: range(mid_index, -1, -1) for left side, range(mid_index + 1, len(nums))
+# for right side, and having mid_sum = middleOutMax(nums, mid_index - 1).
+#
+# Instead, if you take range(mid_index - 1, -1, -1) for left side, and
+# range(mid_index, len(nums)) for right side, setting
+# mid_sum = middleOutMax(nums, mid_index) is fine.
+
 print(maxSubArrDivideAndConquer([-1, 2, 1]))
 print(maxSubArrDivideAndConquer([1, 2, -1]))
 print(maxSubArrDivideAndConquer([-1, -1, 1, 2, 1]))
