8136c1c61c50430b8b859f17c54df8a2753944a7
KangerDrew <kangdrew95@gmail.com>
2022-06-29 14:21:24 -0700

    added pruning to make function run faster

--- matrix/wordSearch.py
+++ matrix/wordSearch.py
@@ -1,4 +1,7 @@
-# My first attempt at word search problem, not passing one of the cases...
+from collections import Counter
+
+
+# My first attempt at word search problem, too slow!
 def wordSearch(board, word):
     rows, columns = len(board), len(board[0])
     w_length = len(word)
@@ -40,14 +43,93 @@ def wordSearch(board, word):
     return False
 
 
-# A    B    C    E
-# S    F    E    S
-# A    D    E    E
-
+# Test case #59
 test_board_1 = [["A", "B", "C", "E"], ["S", "F", "E", "S"], ["A", "D", "E", "E"]]
 string_1 = "ABCESEEEFS"
 
+# Test case #47
 test_board_2 = [["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"],
                 ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "B"], ["A", "A", "A", "A", "B", "A"]]
 string_2 = "AAAAAAAAAAAAABB"
-print(wordSearch(test_board_2, string_2))
+
+
+# print(wordSearch(test_board_2, string_2))
+
+
+def wordSearchWPruning(board, word):
+    rows, columns = len(board), len(board[0])
+
+    # Pruning 1: Check if there's even enough words in the boards:
+    if len(word) > rows * columns:
+        return False
+
+    # Pruning 2: Use python's Counter object to check if enough
+    # letters exist in the board for us to be able to build the word:
+    count = Counter()
+    for n in range(rows):
+        for m in range(columns):
+            count[board[n][m]] += 1
+
+    for c in word:
+        if count[c]:
+            count[c] -= 1
+        else:
+            return False
+
+    # LEETCODE SPECIFIC PRUNING - INVALID ELSEWHERE:
+    # The below "pruning" works to speed up the test case #47,
+    # where we have a board filled with A's and two B's, and our
+    # given string is AAAAAAAAAAAAABB. If we flip the word and
+    # check starting with B first, the recursive function will not
+    # run more than 1 level because there are no sections on the
+    # board where there are two adjacent Bs!
+    if word[0] in count and word[-1] in count:
+        if count[word[0]] > count[word[-1]]:
+            word = word[::-1]
+
+    travelled = set()
+
+    def dfs(r, c, i):
+        if i == len(word):
+            return True
+
+        if (r < 0 or c < 0 or
+                r >= rows or c >= columns or
+                word[i] != board[r][c] or
+                (r, c) in travelled):
+            return False
+
+        travelled.add((r, c))
+        res = (dfs(r + 1, c, i + 1) or
+               dfs(r - 1, c, i + 1) or
+               dfs(r, c + 1, i + 1) or
+               dfs(r, c - 1, i + 1))
+
+        travelled.remove((r, c))
+        return res
+
+    for row in range(rows):
+        for column in range(columns):
+            if dfs(row, column, 0):
+                return True
+
+    return False
+
+
+print(wordSearchWPruning(test_board_2, string_2))
+
+
+test_counter = Counter()
+
+test_counter["Breh"] += 23
+
+print(test_counter)
+
+test_counter["Breh"] -= 21
+
+print(test_counter)
+
+test_counter["Breh"] -= 2
+
+print(test_counter)
+
