e5a8670857a928938d2093ddc00bd8c5ab73f99c
KangerDrew <kangdrew95@gmail.com>
2022-06-30 19:28:44 -0700

    removed duplicate function

--- intervals/nonOverlappingIntervals.py
+++ intervals/nonOverlappingIntervals.py
@@ -20,25 +20,3 @@ def nonOverlappingIntervals(intervals):
 
     return count
 
-
-def nonOverlappingIntervals(intervals):
-
-    count = 0
-    intervals.sort(key=lambda elem: elem[0])
-    currentMin = float('-inf')
-
-    filtered = []
-    for interval in intervals:
-
-        if interval[0] == currentMin:
-            count += 1
-            pass
-        else:
-            currentMin = interval[0]
-            filtered.append(interval)
-
-    for interval in filtered:
-        # GAVE UP HERE
-        pass
-
-    return count
