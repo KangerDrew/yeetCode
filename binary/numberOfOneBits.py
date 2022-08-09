0b91c2b55556e7e6370d0781a42d88cc120456d6
KangerDrew <kangdrew95@gmail.com>
2022-08-08 22:23:27 -0700

    first solution added

--- binary/numberOfOneBits.py
+++ binary/numberOfOneBits.py
@@ -2,3 +2,30 @@ some_string = "011101"
 some_val = 8
 # print(some_string >> 1)
 print(some_val >> 2)
+
+
+# First solution uses modulo function, where we check if modulo 2 of input is non-zero.
+# If it is, we add 1 to the counter. Then, we right shift our input and see if modulo of
+# that is non-zero. We continue to do this until the shifted input value is zero:
+
+# Ex:   100101 ==> +1
+#        10010 ==> N/A
+#         1001 ==> +1
+#          100 ==> N/A
+#           10 ==> N/A
+#            1 ==> +1
+# Solution: 3
+
+def numOfOneBits(n):
+
+    counter = 0
+
+    while n != 0:
+        # Check if rightmost bit value is 1
+        if n % 2 != 0:
+            counter += 1
+
+        # Rightshift
+        n = n >> 1
+
+    return counter
