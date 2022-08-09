e8556fd1c35d9ba610bfdb19b92a6047876b69c1
KangerDrew <kangdrew95@gmail.com>
2022-08-08 20:45:09 -0700

    More comments

--- binary/sumOfTwoIntegers.py
+++ binary/sumOfTwoIntegers.py
@@ -1,11 +1,16 @@
 # For this bit manipulation problem, we can break it down to 3 bit-shifting operators:
 
-# & Operator - Only true when both quantities are true (used for carrying over extra 1)
-
 # XOR Operator - True when only one of them is true, which is also when they're
 # not equal (used for simulating addition)
 
-# Leftshift Operator - Shifting
+# & Operator - Only true when both quantities are true (used for carrying over extra 1)
+
+# Leftshift Operator - Shifting a binary digit to the left (used in conjunction w & operator for
+# calculating a carrying over value)
+
+# Step 1: Use XOR Operator on two values, which will yield a binary value
+# Step 2: Use & AND Leftshift operator on two values, which will give us a carrying over value to be added
+# Step 3: Repeat step 1 and 2 with the values from each step until step 2 returns zero value.
 
 def getSumPython(a, b):
 
