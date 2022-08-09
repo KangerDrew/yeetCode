4fd0058134b111eaf3d662a13cc8da6a5f3221e8
KangerDrew <kangdrew95@gmail.com>
2022-08-08 19:08:57 -0700

    Replacing XOR operator w not equal operator does not work

--- binary/sumOfTwoIntegers.py
+++ binary/sumOfTwoIntegers.py
@@ -8,18 +8,16 @@ def getSumPython(a, b):
 
     mask = 0xffffffff
 
-    print(mask)
-
     while b != 0:
         tmp = (a & b) << 1
-        a = (a ^ b) & mask
+        a = (a != b) & mask
         b = tmp & mask
 
     if a > mask // 2:
-        return ~(a ^ mask)
+        return ~(a != mask)
     else:
         return a
 
 
-print(getSum(2, 3))
+print(getSumPython(2, 3))
 
