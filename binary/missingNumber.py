69588caf7cad6c2c72179086dba4a968744ab9ea
KangerDrew <kangdrew95@gmail.com>
2022-08-09 20:11:14 -0700

    Writing up solution for first solution

---
+++ binary/missingNumber.py
@@ -0,0 +1,18 @@
+# Besides the brute-force method, linear time solution can be achieved
+# by using hashset to store all the values from the input, looping through
+# the range to see which numbers are available. However, this also requires
+# constant space memory for the hashset. There are two solutions that can
+# solve this problem using constant space AND linear time.
+
+# Solution 1: Bit Manipulation
+
+# When we perform XOR bit operation on two identical numbers, we get zero:
+# Ex:     1 0 0 0 1 0
+#         1 0 0 0 1 0
+#       --------------
+#     ==> 0 0 0 0 0 0
+
+#
+
+
+# Solution 2: Gauss Formula Sum - Actual Sum
