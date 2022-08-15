20f460f66481fbcef4c38cc845687533079a934e
KangerDrew <kangdrew95@gmail.com>
2022-08-15 16:56:48 -0700

    First heap problem yay

---
+++ heap/kthLargestElementInArray.py
@@ -0,0 +1,7 @@
+# The easiest solution would be to simply sort the input array,
+# and return the kth last element from the array to get the kth
+# largest element. Time complexity would be O(n log n). But we
+# can do better, using max heap data structure.
+
+
+
