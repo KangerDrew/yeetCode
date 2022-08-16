0253dec96d0c0aa93417af318a66b97fadc9c7db
KangerDrew <kangdrew95@gmail.com>
2022-08-16 11:40:10 -0700

    Explanations added

--- heap/lastStoneWeight.py
+++ heap/lastStoneWeight.py
@@ -9,10 +9,15 @@ def lastStoneWeight(stones):
     heapq.heapify(stones)
 
     while len(stones) > 1:
+        # Pop the two "largest" (largest negative values) from the heap:
         stone_1 = heapq.heappop(stones)
         stone_2 = heapq.heappop(stones)
 
+        # If two values are different, push the negative abs of the difference
+        # into the heap. Otherwise the two stones are destroyed:
         if stone_1 != stone_2:
             heapq.heappush(stones, abs(stone_1 - stone_2) * -1)
 
+    # Return the only remaining stone in the list, or zero
+    # if there are no stones remaining in the list:
     return -stones[0] if len(stones) > 0 else 0
