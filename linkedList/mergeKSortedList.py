7b786ae54950eb94a3fb795591c72ddf3468b9bb
KangerDrew <kangdrew95@gmail.com>
2022-08-18 22:20:16 -0700

    Explanation added

--- linkedList/mergeKSortedList.py
+++ linkedList/mergeKSortedList.py
@@ -37,15 +37,26 @@ def mergeKLists(lists):
     if not lists or len(lists) == 0:
         return None
 
+    # We will use while loop to repeatedly perform merge two lists
+    # at a time, until only 1 remains:
     while len(lists) > 1:
+        # create a new array that will contain an updated lists:
         mergedLists = []
 
+        # Loop through the original lists, but increment by 2:
         for i in range(0, len(lists), 2):
+            # Get node 1 and node 2:
             l1 = lists[i]
+            # node 2 may not exist, if i + 1 is out of bounds:
             l2 = lists[i + 1] if i + 1 < len(lists) else None
 
+            # Use mergeList helper function to merge l1 and l2, and
+            # append it to the mergedLists array:
             mergedLists.append(mergeList(l1, l2))
 
+        # Replace lists with updated mergedLists object
         lists = mergedLists
 
+    # There should only be one list remaining in the lists after
+    # the while loop. Return it:
     return lists[0]
