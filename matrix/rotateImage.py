45e6ecb380fd0e6fdc3ca5bcd65d762337a8a39d
KangerDrew <kangdrew95@gmail.com>
2022-08-24 21:52:28 -0700

    Function setup

--- matrix/rotateImage.py
+++ matrix/rotateImage.py
@@ -1,2 +1,9 @@
-#
-#
+# The function will not return anything,
+# it'll simply mutate the input matrix
+
+def rotate(matrix):
+
+    # Get the left & right bound of the matrix:
+    l, r = 0, len(matrix) - 1
+
+
