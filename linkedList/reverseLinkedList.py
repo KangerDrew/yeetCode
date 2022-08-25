06eac9d147992bdd17d7626181fcf8038223a989
KangerDrew <kangdrew95@gmail.com>
2022-08-24 23:47:11 -0700

    Correct solution added, need comment

--- linkedList/reverseLinkedList.py
+++ linkedList/reverseLinkedList.py
@@ -21,3 +21,26 @@ def reverse(head):
             return current
 
 
+def reverseCORRECTED(head):
+    # Edge case #1 - List is empty
+    if not head:
+        return None
+
+    # Edge case #2 - List only consists of one node:
+    if not head.next:
+        return head
+
+    pre = None
+    current = head
+    after = head.next
+
+    while current:
+
+        current.next = pre
+        pre = current
+
+        if not after:
+            return current
+
+        current = after
+        after = after.next
