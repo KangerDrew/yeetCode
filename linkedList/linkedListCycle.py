c6ad80070d8e83020dccce84f3cff234f01c23c7
KangerDrew <kangdrew95@gmail.com>
2022-08-25 00:04:18 -0700

    First attempt solution added

---
+++ linkedList/linkedListCycle.py
@@ -0,0 +1,24 @@
+# My first attempt at detecting linked list cycle problem:
+
+def hasCycle(head):
+    # Edge case - Nothing in Linked List, means no cycle
+    if not head:
+        return False
+
+    pointer1 = head
+    pointer2 = head
+
+    while pointer2:
+
+        pointer1 = pointer1.next
+        pointer2 = pointer2.next
+
+        if not pointer2:
+            return False
+
+        pointer2 = pointer2.next
+
+        if pointer1 == pointer2:
+            return True
+
+    return False
