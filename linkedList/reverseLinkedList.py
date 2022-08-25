e0e2dd4c5d5967bdbc370dbafde8315ea6de0300
KangerDrew <kangdrew95@gmail.com>
2022-08-24 23:46:21 -0700

    Wrong first attempt solution

--- linkedList/reverseLinkedList.py
+++ linkedList/reverseLinkedList.py
@@ -1,13 +1,23 @@
 # My first blind attempt at reverse linked list:
 
+# WRONG
 def reverse(head):
     # Edge case - List only consists of one node:
     if not head.next:
         return head
 
     pre = None
-
+    current = head
     after = head.next
 
-    return head
+    while current:
+
+        current.next = pre
+        pre = current
+        current = after
+        after = after.next
+
+        if not after:
+            return current
+
 
