b2b98f4aa8006bf52a842af09bcc83530eba2ff1
KangerDrew <kangdrew95@gmail.com>
2022-08-26 20:56:31 -0700

    how to solve the problem

---
+++ intervals/meetingRooms.py
@@ -0,0 +1,4 @@
+# This problem asks if there are any overlaps in the provided intervals.
+# Approach is very similar to mergeIntervals problem, but we just simply
+# return True/False depending on whether we find an overlap or not:
+
