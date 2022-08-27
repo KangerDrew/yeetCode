f6d4b1ee27f36dc76947cced7994c73519f51da5
KangerDrew <kangdrew95@gmail.com>
2022-08-26 19:58:54 -0700

    more comments needed

--- intervals/insertIntervals.py
+++ intervals/insertIntervals.py
@@ -5,11 +5,17 @@ def insertIntervals(intervals, newInterval):
     # Side note 1: I tried switching the order of the two initial if and elif
     # statements, and both resulted in correct answer!
     for i in range(len(intervals)):
-        if newInterval[0] > intervals[i][1]:
-            result.append(intervals[i])
-        elif newInterval[1] < intervals[i][0]:
+        # Check if last value of the newInterval is less than the first value of current
+        # interval. If so, it means that remaining intervals come after
+        if newInterval[1] < intervals[i][0]:
             result.append(newInterval)
             return result + intervals[i:]
+        # Check if the initial value of the newInterval is greater than the last value
+        # of the
+        elif newInterval[0] > intervals[i][1]:
+            result.append(intervals[i])
+        # If neither statement are true, that means we need to update the current newInterval
+        # by merging it with the current intervals[i]:
         else:
             newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
     # If we completed the for loop without returning early,
