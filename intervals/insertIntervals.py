f4ecf6a1ca739abc3d5a70e8c7a66f9f2337d765
KangerDrew <kangdrew95@gmail.com>
2022-08-26 19:51:42 -0700

    Reviewing insert intervals

--- intervals/insertIntervals.py
+++ intervals/insertIntervals.py
@@ -2,12 +2,14 @@ def insertIntervals(intervals, newInterval):
 
     result = []
 
+    # Side note 1: I tried switching the order of the two initial if and elif
+    # statements, and both resulted in correct answer!
     for i in range(len(intervals)):
-        if newInterval[1] < intervals[i][0]:
+        if newInterval[0] > intervals[i][1]:
+            result.append(intervals[i])
+        elif newInterval[1] < intervals[i][0]:
             result.append(newInterval)
             return result + intervals[i:]
-        elif newInterval[0] > intervals[i][1]:
-            result.append(intervals[i])
         else:
             newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
     # If we completed the for loop without returning early,
