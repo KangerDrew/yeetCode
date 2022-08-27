d134766f41ab32e3be2e573561aa0028affc9241
KangerDrew <kangdrew95@gmail.com>
2022-08-26 20:03:44 -0700

    added helpful comments

--- intervals/insertIntervals.py
+++ intervals/insertIntervals.py
@@ -6,18 +6,23 @@ def insertIntervals(intervals, newInterval):
     # statements, and both resulted in correct answer!
     for i in range(len(intervals)):
         # Check if last value of the newInterval is less than the first value of current
-        # interval. If so, it means that remaining intervals come after
+        # intervals[i]. If so, it means we can append the newInterval to the result, then,
+        # append the remaining intervals to the result (exit out of for loop and the function
+        # by returning the result here!):
         if newInterval[1] < intervals[i][0]:
             result.append(newInterval)
             return result + intervals[i:]
-        # Check if the initial value of the newInterval is greater than the last value
-        # of the
+        # Check if the initial value of the newInterval is greater than the last value of the
+        # current intervals[i]. If true, we append the current intervals[i] to the result. We
+        # don't know if the next interval will overlap with newInterval, so we proceed with the
+        # for loop:
         elif newInterval[0] > intervals[i][1]:
             result.append(intervals[i])
         # If neither statement are true, that means we need to update the current newInterval
-        # by merging it with the current intervals[i]:
+        # by merging it with the current intervals[i]. Proceed with the for loop:
         else:
             newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+
     # If we completed the for loop without returning early,
     # it means the newInterval belongs to the very end of the
     # intervals:
