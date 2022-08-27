8db8a4ad070503ea3deb072f9af5cdcba8999593
KangerDrew <kangdrew95@gmail.com>
2022-08-26 20:57:34 -0700

    Problem solved. Need comments

--- intervals/meetingRooms.py
+++ intervals/meetingRooms.py
@@ -2,3 +2,14 @@
 # Approach is very similar to mergeIntervals problem, but we just simply
 # return True/False depending on whether we find an overlap or not:
 
+def meetingRooms(intervals):
+    # Sort the array, based on the first value of the interval:
+    intervals.sort(key=lambda x: x[0])
+
+    for i in range(1, len(intervals)):
+
+        if intervals[i - 1][1] > intervals[i][0]:
+            return False
+
+    return True
+
