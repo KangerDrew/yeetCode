7e6510c72c600b82a05c5e9cd3876baf488c75bf
KangerDrew <kangdrew95@gmail.com>
2022-08-29 22:22:08 -0700

    explanation for my solution added

--- intervals/nonOverlappingIntervals.py
+++ intervals/nonOverlappingIntervals.py
@@ -27,16 +27,28 @@ def nonOverlappingIntervals(intervals):
 
 def nonOverlappingIntervalsAttempt(intervals):
 
+    # Sort:
     intervals.sort(key=lambda i: i[0])
+
+    # count will keep track of how many "intervals" would have to be removed:
     count = 0
+    # recentEnd will keep track of the "previous interval's end value", which
+    # we need in order to determine which interval is to be removed:
     recentEnd = intervals[0][1]
 
+    # Iterate from the 2nd element of the intervals list:
     for j in range(1, len(intervals)):
 
+        # Check whether the beginning of the interval conflicts with the previous end:
         if intervals[j][0] < recentEnd:
+            # If there was a conflict, that means an interval needs to be removed:
             count += 1
+            # We need to "remove" the interval with the greater end value. We can simulate
+            # this by updating our recentEnd value by taking the smaller one:
             recentEnd = min(recentEnd, intervals[j][1])
         else:
+            # If there were no conflicts, then we don't remove any interval and simply update
+            # the recentEnd value with our current interval's end value:
             recentEnd = intervals[j][1]
 
     return count
