52a2fc2de2d36e335f6c5eb2f4fd703300f58a3e
KangerDrew <kangdrew95@gmail.com>
2022-08-29 22:15:30 -0700

    my first blind attempt at solution

--- intervals/nonOverlappingIntervals.py
+++ intervals/nonOverlappingIntervals.py
@@ -24,3 +24,20 @@ def nonOverlappingIntervals(intervals):
 # The approach for non-overlapping interval is to sort the provided
 # interval list by their first interval value, and keep track of a count
 # value...
+
+def nonOverlappingIntervalsAttempt(intervals):
+
+    intervals.sort(key=lambda i: i[0])
+    count = 0
+    recentEnd = intervals[0][1]
+
+    for j in range(1, len(intervals)):
+
+        if intervals[j][0] < recentEnd:
+            count += 1
+            recentEnd = min(recentEnd, intervals[j][1])
+        else:
+            recentEnd = intervals[j][1]
+
+    return count
+
