8178f9f2376533ba6246bfc9da1b1397945cd89e
KangerDrew <kangdrew95@gmail.com>
2022-08-30 21:09:26 -0700

    More explanation

--- dynamicProgramming/maximalSquare.py
+++ dynamicProgramming/maximalSquare.py
@@ -4,5 +4,9 @@
 # (how it's done in leetcode solution, but not how it's done in neetcode vid)
 
 # If we do TLBR, the rightmost column and topmost row's largest square value
-# will
+# will simply be either be 0 or 1. (can't check beyond it). From there, we iterate
+# from 1, 1 position, and see what the previous top, previous left, and previous
+# topleft largest square value was. The minimum between these values, and the
+# current value (whether it's 1 or 0) will determine the largest possible square
+# length at that given position, when excluding the remaining matrix at the bottom
 
