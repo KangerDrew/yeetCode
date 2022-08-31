1e8a8975888519c1614f04659e6f16aa1da2563a
KangerDrew <kangdrew95@gmail.com>
2022-08-30 23:18:51 -0700

    Comments added to approach 2

--- intervals/meetingRoomsII.py
+++ intervals/meetingRoomsII.py
@@ -45,29 +45,42 @@ def meetingRoomsIIHeap(intervals):
 
 # Method 2 - Assess beginning and ending separately:
 # This requires the beginning and end to be sorted separately!!
+# But the principal is similar to the previous problem. We add
+# new meeting into the count by checking when the earliest ending
+# would be:
 
 def meetingRoomsII(intervals):
 
     beginningTimes = [i[0] for i in intervals]
     endingTimes = [j[1] for j in intervals]
 
+    # The two lists needs to be sorted individually!
     beginningTimes.sort()
     endingTimes.sort()
 
+    # counters:
     maxCount, currentCount = 0, 0
-
+    # Pointers for the two lists:
     b, e = 0, 0
 
+    # Loop until we run out of meetings to add:
     while b < len(intervals):
 
+        # Compare the current beginning value with our earliest ending time:
         if beginningTimes[b] < endingTimes[e]:
+            # If the earliest ending time conflicts with the current beginning time,
+            # we increment the current meeting count value and move onto the next meeting
             b += 1
             currentCount += 1
         else:
+            # If it doesn't conflict, it means we can remove a meeting from the currentCount.
+            # Increment the e counter to update the "earliest ending time" and decrement currentCount:
             e += 1
             currentCount -= 1
 
+        # Compare maxCount to currentCount:
         maxCount = max(maxCount, currentCount)
 
+    # Return the maxCount
     return maxCount
 
