4d1031185b66150f1c9f59095f6af5e70d8ef3f3
KangerDrew <kangdrew95@gmail.com>
2022-08-30 21:02:01 -0700

    writing my own solution

---
+++ dynamicProgramming/maximalSquare.py
@@ -0,0 +1,8 @@
+# The idea behind solving this problem is to use memoization (cache) to
+# track the largest square length at a given position. For my implementation,
+# I will calculate the largest squares from top left to bottom right (TLBR).
+# (how it's done in leetcode solution, but not how it's done in neetcode vid)
+
+# If we do TLBR, the rightmost column and topmost row's largest square value
+# will
+
