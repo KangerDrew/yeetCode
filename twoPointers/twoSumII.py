632c6a9c80793f314548ba391f88dada58ffd246
KangerDrew <kangdrew95@gmail.com>
2022-09-06 15:40:38 -0700

    NOT binary search, but using two pointers

--- twoPointers/twoSumII.py
+++ twoPointers/twoSumII.py
@@ -1,5 +1,13 @@
 # This is a variant of a popular problem two sums, except the given
 # array is sorted...
 
-# Sorted array indicates the use of binary search!
+# Sorted array indicates the use of two pointers (not binary search)!
+# One pointer will begin at the beginning of array, another will begin
+# at the end of the array.
 
+# We increment the beginning pointer if the two sum is too small, and
+# we increment the end pointer if the two sum is too large!
+
+# Add +1 to the index value when returning... Not sure why...
+
+def twoSumsII(numbers):
