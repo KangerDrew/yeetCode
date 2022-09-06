9fcd6a995cd92a8415e84c87809c3de3c468e7b0
KangerDrew <kangdrew95@gmail.com>
2022-09-06 16:20:45 -0700

    approach explanation for 3sums problem

---
+++ twoPointers/3sums.py
@@ -0,0 +1,18 @@
+# The 3sum problem can be broken down into multiple two sum II problems.
+# But first, we sort the input array!
+
+
+# There will be three pointers in this problem - start, left, and right.
+
+# Starting Pointer - The pointer that determines the first value we use to
+# check for possible 3 sum combo.
+# Left Pointer - This pointer is the "start" pointer from two sums II problem
+# Right Pointer - This is the "end" pointer
+
+# Once we select the start pointer, we use the remaining values to the right
+# to conduct a two sum II problem, where the two sum value must be the "opposite
+# number" to that of the one in the starting pointer!
+
+# NOTE - if value at the start pointer exceeds 0, that means we can finish
+# looking for possible combo since all the values remaining for testing will
+# always return sum greater than zero!
