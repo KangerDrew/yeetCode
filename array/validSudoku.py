bd8e227c8374017015bdbc346f0bf21033af165c
KangerDrew <kangdrew95@gmail.com>
2022-09-05 20:30:10 -0700

    How to handle rule 1 and 2

--- array/validSudoku.py
+++ array/validSudoku.py
@@ -1,3 +1,9 @@
-# We can use a fresh new hashset set to validate whether
-# each row/column is valid sudoku. However, checking each
-# 3x3 sub-boxes will be slightly more tricky...
+# For rules 1 & 2:
+# We can use a hashset set to validate whether each row/column is valid
+# sudoku. To make the algorithm runtime efficient, we will be using two
+# default dictionaries (hash map), where the keys will be the index of
+# column/row, and the key value will be a hash set that keeps track of
+# what number was on a row/column.
+
+# For rule 3:
+# Like we did for previous rules...
