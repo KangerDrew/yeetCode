50fd5a7ab74e533f88a8179b44968e86971924ee
KangerDrew <kangdrew95@gmail.com>
2022-09-05 20:58:07 -0700

    rule 3 explanation added

--- array/validSudoku.py
+++ array/validSudoku.py
@@ -6,4 +6,12 @@
 # what number was on a row/column.
 
 # For rule 3:
-# Like we did for previous rules...
+# We need to figure out a way to keep track of each individual 3x3 sub-
+# boxes. To do this, we divide up our 9x9 board. IMPORTANT - Notice how
+# when we take the floor division 3 of the column/row index, it can tell
+# us which 3x3 sub-box that coordinate belongs to (see video for visual
+# explanation). Using this, we can keep track of which sub-box contains
+# certain number. We will use the (row_index // 3, column_index // 3) tuple,
+# for the key of our default dictionary!
+
+
