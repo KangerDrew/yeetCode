532273781382ca52f9379130cef2bd476998bf62
KangerDrew <kangdrew95@gmail.com>
2022-09-07 22:15:43 -0700

    Explaining memory efficient solution

--- linkedList/intersectionOfTwoLists.py
+++ linkedList/intersectionOfTwoLists.py
@@ -24,3 +24,24 @@ def intersectionHashSet(headA, headB):
 
 # There is a way to solve this problem without using hash set (O(1) memory)
 # It involves the use of two pointers...
+
+# Example:
+# A1 => A2 =>
+#             C1 => C2 => C3 => NONE
+#       B1 =>
+
+# We can visualize the above intersecting linked list as two separate lists:
+# A1 => A2 => C1 => C2 => C3 => NONE
+# <    A    > <       C       >
+# B1 => C1 => C2 => C3 => NONE
+# < B > <       C       >
+# Let's label comp
+
+# Now, try adding two separate lists, twice in different order:
+# A1 => A2 => C1 => C2 => C3 => B1 => C1 => C2 => C3 => NONE
+# B1 => C1 => C2 => C3 => A1 => A2 => C1 => C2 => C3 => NONE
+# <    A    > < B > <       C       >
+
+# The length of each lists is: A + B + 2C
+# Note that the intersection occurs C distance from the end of the list.
+
