bf5749b46af3fb5215ea14a60b68c7157ae3a0ab
KangerDrew <kangdrew95@gmail.com>
2022-09-10 02:32:05 -0700

    explanation added

--- dynamicProgramming/uniquePathsII.py
+++ dynamicProgramming/uniquePathsII.py
@@ -1,7 +1,18 @@
 # Coped & modified from the original variant problem:
 
-def uniquePathsIIBottomUp(obstacleGrid):
+# The introduction of "obstacle" means we need to make following adjustments
+# to the original solution:
 
+# 1) We are given a grid instead of just the dimension. Get the dimension.
+# 2) In our for loop, we need to introduce an if condition to skip over the
+# loop if there is an obstacle, as it would mean there's 0 ways to reach that
+# position of the grid.
+# 3) Edge case - If the starting point is an obstacle, that means the grid is
+# impossible to traverse. Immediately return 0.
+
+def uniquePathsIIBottomUp(obstacleGrid):
+    # Edge case - If the starting point is an obstacle, that means the grid is
+    # impossible to traverse. Immediately return 0.
     if obstacleGrid[0][0] == 1:
         return 0
 
