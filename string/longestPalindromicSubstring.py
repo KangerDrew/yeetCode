52c2636d149e9046c574cbf7c10b551e82ca1fb6
KangerDrew <kangdrew95@gmail.com>
2022-09-15 23:11:57 -0700

    working on odd length palindrome solution

--- string/longestPalindromicSubstring.py
+++ string/longestPalindromicSubstring.py
@@ -10,3 +10,22 @@
 # longest EVEN number length palindrome!
 
 
+def longestPalindromeCenter(s):
+
+    max_pal = ""
+    str_len = 0
+
+    for i in range(len(s)):
+
+        # Odd length palindrome check:
+        left, right = i, i
+
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+
+        if (right - left) + 1 > str_len:
+            # Remember, the right boundary is non-inclusive so we
+            # need to get string from left, to "right + 1"
+            max_pal = s[left:(right + 1)]
+
