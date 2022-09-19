797665fe8b6940edab1768d16ee199b765e7b371
KangerDrew <kangdrew95@gmail.com>
2022-09-19 14:10:41 -0700

    lambda function explained

--- heap/topKFrequentWords.py
+++ heap/topKFrequentWords.py
@@ -9,6 +9,10 @@ def topKFreqWords(words, k):
 
     tuple_count = list(count.items())
 
+    # Notice how the lambda function in key uses a tuple.
+    # When getting sorted, the nsmallest will return items in order of
+    # largest negative frequency (small), then will take lexicographical
+    # order of the word into the account:
     unprocessed = heapq.nsmallest(k, tuple_count, key=lambda a: (-a[1], a[0]))
 
     res = []
