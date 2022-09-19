9a1c5e47ece28a0a5841d7da0c7e97b41a27152a
KangerDrew <kangdrew95@gmail.com>
2022-09-18 19:49:26 -0700

    Slow solution added. Does not pass test cases

--- math/powXN.py
+++ math/powXN.py
@@ -2,3 +2,15 @@
 # specified amount, and return it... However we need much faster
 # solution than this!
 
+def powSlow(x, n):
+
+    sol = 1
+
+    while n > 0:
+        sol *= x
+        n -= 1
+
+    return sol
+
+
+print(powSlow(3,2))
