70daeb6721b72a2fbc629d8dfe38a5f1296e174e
KangerDrew <kangdrew95@gmail.com>
2022-09-18 19:54:10 -0700

    Added comment to the slow solution

--- math/powXN.py
+++ math/powXN.py
@@ -4,13 +4,17 @@
 
 def powSlow(x, n):
 
+    # Initialize a variable we'll use to return the answer
     sol = 1
+    # Get the absolute (positive) value of n:
     power = abs(n)
 
+    # Loop until we finished multiplying by abs(n) times
     while power > 0:
         sol *= x
         power -= 1
 
+    # If n was positive, return sol. If not, return the inverse:
     return sol if n > 0 else 1 / sol
 
 
