2705ba4c37e236255d9f0da990d67dfde799f7d9
KangerDrew <kangdrew95@gmail.com>
2022-09-24 21:12:57 -0700

    sorting solution described

--- linkedList/findTheDuplicateNumber.py
+++ linkedList/findTheDuplicateNumber.py
@@ -7,9 +7,18 @@
 # Condition 3 - All integers in nums appear ONLY ONCE except for
 # PRECISELY ONE INTEGER which appears two or more times.
 
+
+# Approach 1: Brute Force
 # Brute force approach would involve looping through the array O(n^2)
 # times, comparing each value with another value in array to see if
 # they're the same. Though this is solution would give us  constant
 # space complexity, it's way too slow...
 
+
+# Approach 2: Sort and check
+# This would involve sorting the given array, and checking for duplicate
+# numbers. Though better than last solution in time complexity, (O(n log n)),
+# this solution would cost us more space complexity, as best sorting
+# algorithms would take O( log n ) space (quick sort), or in python's case,
+# it would take O(n) space complexity in worst-case scenario
 
