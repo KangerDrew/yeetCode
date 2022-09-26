c7dc8547c6fc15981812dc6739f594a3fa614913
KangerDrew <kangdrew95@gmail.com>
2022-09-25 20:39:11 -0700

    adding explanation for o(n) solution w extra memory

--- array/productExceptSelf.py
+++ array/productExceptSelf.py
@@ -13,6 +13,21 @@ def productExceptSelf(nums):
     return answer
 
 
+# The simplest solution that runs at O(n) without the use of division
+# requires a use of two arrays - one containing all the multiples
+# prior to the index (prefix) and another containing the multiples
+# after the index (postfix).
+
+# Ex. [1, 2, 3, 4]
+
+# Prefix:   [1, 1*2, 1*2*3, 1*2*3*4]
+#            ======================>
+# Postfix:  [4*3*2*1, 4*3*2, 4*3, 4]
+#            <======================
+
+# For a given index i, if we want to get the
+
+
 def productExceptSelfFaster(nums):
     answer = [1]
 
