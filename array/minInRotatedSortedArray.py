22d63931b2760ecf2cd5998ec64c7a6eb79b2967
KangerDrew <kangdrew95@gmail.com>
2022-09-26 21:56:18 -0700

    adding explanation for problem

--- array/minInRotatedSortedArray.py
+++ array/minInRotatedSortedArray.py
@@ -5,14 +5,23 @@ def findMin(nums):
     while left <= right:
         leftVal = nums[left]
         rightVal = nums[right]
+
+        # If the current sub-array we're analyzing is sorted
+        # from beginning to the end, that means we've potentially
+        # discovered the smallest value in rotated array. Compare
+        # it against the current minimum value, and break out of
+        # the while loop:
         if leftVal < rightVal:
             res = min(res, leftVal)
             break
 
         middle = (left + right) // 2
         midVal = nums[middle]
+
+        # Compare the recorded min value against value at midpoint:
         res = min(res, midVal)
 
+        # Two possible scenario:
         if midVal >= leftVal:
             left = middle + 1
         else:
