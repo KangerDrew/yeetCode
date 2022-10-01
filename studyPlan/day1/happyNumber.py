2ecf888847c12d496a94650d5530bab1b4b47bd8
KangerDrew <kangdrew95@gmail.com>
2022-10-01 14:26:21 -0700

    first atempt solution added

--- studyPlan/day1/happyNumber.py
+++ studyPlan/day1/happyNumber.py
@@ -4,3 +4,26 @@
 # that particular sum value. Infinite loop will occur when during the summing of squared
 # digits, we run into a number that we've already processed!
 
+
+def happyNum(n):
+
+    covered = set()
+    newVal = n
+
+    while newVal not in covered:
+
+        covered.add(newVal)
+        temp = 0
+        newValArr = [int(i) for i in str(newVal)]
+
+        for val in newValArr:
+            temp += val ** 2
+
+        if temp == 1:
+            return True
+
+        newVal = temp
+
+    return False
+
+
