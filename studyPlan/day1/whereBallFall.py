4195997c20314d29690912a0172502709d1c1f81
KangerDrew <kangdrew95@gmail.com>
2022-10-01 19:25:04 -0700

    Iterative solution added

--- studyPlan/day1/whereBallFall.py
+++ studyPlan/day1/whereBallFall.py
@@ -25,7 +25,6 @@
 # Time Complexity - O(m * n)
 # Space Complexity - O(m), since we enter at most m recursive stack
 def whereBallFallDFS(grid):
-
     def dfs(row, col):
         # Base case where ball reached the bottom:
         if row == len(grid):
@@ -54,5 +53,36 @@ def whereBallFallDFS(grid):
 # Iterative solution, better than dfs approach since space complexity is
 # going to be O(1) because there's no recursion stack:
 def whereBallFallIter(grid):
+    res = []
 
-    return "stuff"
+    for col in range(len(grid[0])):
+
+        # Need variable to keep track of ball position
+        currentCol = col
+        # Need boolean to check that ball didn't get stuck
+        success = True
+        for row in range(len(grid)):
+
+            # Like previous problem, nextCol is the column position that ball
+            # should end up given the current slant orientation:
+            nextCol = currentCol + grid[row][currentCol]
+
+            # 1 - If the column is out of bounds, ball will be stuck.
+            # 2 - If the slant at [row][nextCol] does not match, the ball will get stuck.
+            if not (0 <= nextCol < len(grid[0])) or grid[row][currentCol] != grid[row][nextCol]:
+                res.append(-1)
+                # ball failed to reach bottom, update boolean:
+                success = False
+                # break out of the current "trajectory"
+                break
+
+            # If above if statement didn't trigger, update currentCol and proceed
+            currentCol = nextCol
+
+        # If the above for loop didn't break, then append the currentCol value to res:
+        if success:
+            res.append(currentCol)
+
+    # Return the array:
+    return res
+
