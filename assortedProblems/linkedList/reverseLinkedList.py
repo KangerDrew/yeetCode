6268b781839a5708063d20896088cbb3493cf809
KangerDrew <kangdrew95@gmail.com>
2022-10-04 15:05:34 -0700

    Reversed linked list using recursion method. Added explanation for clarity

--- assortedProblems/linkedList/reverseLinkedList.py
+++ assortedProblems/linkedList/reverseLinkedList.py
@@ -69,21 +69,52 @@ def reverseShorter(head):
 
 # Recursive solution:
 def reverseRecursive(head):
+    # Edge case in case empty linked list is entered:
     if not head:
         return head
 
-
+    # The purpose of the recursive function is to traverse deep into
+    # the linked list until the end is reached. From there, as each
+    # recursion stack is removed, we'll be reversing the direction of
+    # each node and return the "end" of the list back to the top:
     def recursive(node):
+        # Return condition here indicates we've reached the end of the
+        # list, and we can return the end:
         if not node.next:
             return node
 
-        rev = recursive(node.next)
+        # If the above statement didn't trigger, we need to traverse
+        # deeper into the linked list to get the end of the list (which
+        # will be the head of the reversed list)
+        last_node = recursive(node.next)
 
-        # Set the .next of the node after back to the input,
-        # creating a mini loop:
+        # With the exception of the case where we're at the very end,
+        # we are in the middle of a linked list. We need to reverse
+        # the direction of the node that comes after. Set the next
+        # value of the next node to be
         node.next.next = node
-        # Set the next pointer of the input to be None.
-        node.next = None
-        return rev
 
-    return recursive(head)
+        # Optionally, we can set the .next value to be Null node at each
+        # stack. But this will not be necessary since at each stack level
+        # after removing this one, we'll be resetting the direction. The
+        # only reason why we'd set the next node to null is so that after
+        # we've reached the very end (back to the original head node), the
+        # original head node will be pointing towards the None, since it's
+        # now the end of the reversed list:
+        # node.next = None
+
+        # Return the recursively obtained last_node:
+        return last_node
+
+    # Use recursive helper function to reverse the list, and get the last
+    # node of the list.
+    end_node = recursive(head)
+    # Now, we still have the reference to the original head. But if we didn't
+    # set "node.next = None" on each recursion stack, we'll currently have a
+    # reversed linked list where the last node is pointing back to the second
+    # last node (mini-cycle at the end). So fix this by setting head.next to
+    # None instead!
+    head.next = None
+
+    # Return the end_node, which is now the new head of the reversed linked list!
+    return end_node
