adfb3e5aa508599a813343c7e971589899ed0738
KangerDrew <kangdrew95@gmail.com>
2022-10-08 15:30:43 -0700

    Easy variant of path sum problem solved

---
+++ studyPlan/day7/pathSumI.py
@@ -0,0 +1,15 @@
+def pathSum(root, targetSum):
+
+    def dfs(node, currentSum):
+        if not node:
+            return False
+
+        currentSum += node.val
+
+        if not node.left and not node.right:
+            return currentSum == targetSum
+
+        return dfs(node.left, currentSum) or dfs(node.right, currentSum)
+
+    return dfs(root, 0)
+
