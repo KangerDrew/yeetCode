da5d9591b1feba02d1077e60ce07b495f2f63431
KangerDrew <kangdrew95@gmail.com>
2022-10-12 01:49:31 -0700

    Reading problem statement

--- studyPlan/day5/taskScheduler.py
+++ studyPlan/day5/taskScheduler.py
@@ -1,1 +1,4 @@
-# Another new problem
+# One possible solution involves the use of max heap, to keep track of which
+# "task" (letter) has the highest count, and prioritizing the elimination of
+# those letters first, and maximize the amount we alter the tasks to reduce
+# the total amount of time spent.
