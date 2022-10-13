577792877adad69e7801efd39130fff5e117ab48
KangerDrew <kangdrew95@gmail.com>
2022-10-13 15:45:19 -0700

    completed problem

---
+++ studyPlan/day11/courseScheduleII.py
@@ -0,0 +1,36 @@
+import collections
+
+
+def courseScheduleII(numCourses, prerequisites):
+
+    prereq = {c: [] for c in range(numCourses)}
+    for course in prerequisites:
+        prereq[course[0]].append(course[1])
+
+
+    schedule = []
+    visit, cycle = set(), set()
+
+    def dfs(crs):
+        if crs in cycle:
+            return False
+
+        if crs in visit:
+            return True
+
+        cycle.add(crs)
+        for pre in prereq[crs]:
+            if not dfs(pre):
+                return False
+
+        cycle.remove(crs)
+        visit.add(crs)
+        schedule.append(crs)
+        return True
+
+    for i in range(numCourses):
+        if not dfs(i):
+            return []
+
+    return schedule
+
