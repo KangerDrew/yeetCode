af34b244291b9899cebdc9e7ef65fc3865d4c967
KangerDrew <kangdrew95@gmail.com>
2022-10-26 19:08:01 -0700

    My first attempt at this problem

--- studyPlanEasy/day2 - String/isSubsequence.py
+++ studyPlanEasy/day2 - String/isSubsequence.py
@@ -1,2 +1,15 @@
+# My first attempt:
 
+def isSubsequence(s, t):
 
+    s_pointer = 0
+
+    for t_string in t:
+
+        if s_pointer == len(s):
+            break
+
+        if s[s_pointer] == t_string:
+            s_pointer += 1
+
+    return s_pointer == len(s)
