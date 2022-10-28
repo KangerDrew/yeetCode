5aee51d4dd6fd2c91b2fa684c762f33658966529
KangerDrew <kangdrew95@gmail.com>
2022-10-27 21:21:10 -0700

    iterative solution from memory

--- studyPlanEasy/day3 - Linked List/reverseLinkedList.py
+++ studyPlanEasy/day3 - Linked List/reverseLinkedList.py
@@ -1,1 +1,24 @@
-# Classic problem
+# Classic problem. Solve it both iteratively and recursively:
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverseLinkedListItr(head):
+    if not head.next:
+        return head
+
+    pre = None
+    current = head
+    post = head.next
+
+    while post:
+        current.next = pre
+        pre = current
+        current = post
+        post = post.next
+
+    current.next = pre
+
+    return current
