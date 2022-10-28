d0791d1363aa0aae10c07412caffa464a2f3df13
KangerDrew <kangdrew95@gmail.com>
2022-10-27 21:45:20 -0700

    Reverse linked list solved recursively, added comments

--- studyPlanEasy/day3 - Linked List/reverseLinkedList.py
+++ studyPlanEasy/day3 - Linked List/reverseLinkedList.py
@@ -44,3 +44,36 @@ def reverseItrImproved(head):
         current = post
 
     return
+
+
+# Recursive solution from memory:
+def reverseRecur(head):
+
+    if not head or not head.next:
+        return head
+
+    def recur(node):
+        # Edge case - We found the end of the original list:
+        if not node.next:
+            return node
+
+        # First, traverse deeper into recursion:
+        new_head = recur(node)
+
+        # Flip the next node's pointer:
+        after = node.next
+        after.next = node
+
+        # Return the "new head" of the reversed list:
+        return new_head
+
+    # Get the new head:
+    final = recur(head)
+
+    # At this point, there's going to be a cycle at the end of the newly
+    # reversed list. Flip it so it points to None. We still have access
+    # to it from "head" variable:
+    head.next = None
+    # Return the head of the reversed list:
+    return final
+
