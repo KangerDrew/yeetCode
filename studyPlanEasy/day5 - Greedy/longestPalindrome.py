fe971f1eb15cae191486ff3d1462bf3a9ce73ddf
KangerDrew <kangdrew95@gmail.com>
2022-10-28 21:41:40 -0700

    slightly more memory efficient solution described

--- studyPlanEasy/day5 - Greedy/longestPalindrome.py
+++ studyPlanEasy/day5 - Greedy/longestPalindrome.py
@@ -1,7 +1,6 @@
-# My first attempt:
 import collections
 
-
+# My first attempt:
 def longestPalindrome(s):
 
     odd_num_letter = False
@@ -20,5 +19,21 @@ def longestPalindrome(s):
 
     if odd_num_letter:
         palindrome_len += 1
+
+    return palindrome_len
+
+
+# Slight optimization, to remove odd_num_letter boolean. Originally this variable
+# was used to keep track of whether we managed to find a unique letter (if the count
+# of that letter is odd) that could be used as the center of palindrome. However,
+# we can instead just check the length of the palindrome that we're keeping!
+
+# If we first encounter a letter with odd number count, we check if our palindrome length
+# is odd or even. If it's odd, it means that we've already added a unique center. Otherwise,
+# if that is a first time we've encountered an odd number count letter, we increment the
+# palindrome count by 1!
+
+def longestPalindromeBetter(s):
+    palindrome_len = 0
 
     return palindrome_len
