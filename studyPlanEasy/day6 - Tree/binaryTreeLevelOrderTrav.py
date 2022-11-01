a237cd21a7b329440c58b1c4d3bab7c8484eede9
KangerDrew <kangdrew95@gmail.com>
2022-11-01 11:15:19 -0700

    added description on how to solve problem using dfs recursion

---
+++ studyPlanEasy/day6 - Tree/binaryTreeLevelOrderTrav.py
@@ -0,0 +1,3 @@
+# You can use iterative BFS solution to solve this problem, but there is
+# a way to solve this using recursive "DFS" solution. You just need to
+# keep track of which level you are entering at each recursion stack...
