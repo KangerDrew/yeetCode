a987d276e231520aea5c804ab9cca999f6ed5694
KangerDrew <kangdrew95@gmail.com>
2022-11-03 16:14:15 -0700

    don't use union find lol

---
+++ studyPlanEasy/day9 - Graph/...numberOfIslands.py
@@ -0,0 +1,7 @@
+# Can be solved simply using dfs/bfs, but I will attempt to solve this using
+# union find algorithm...
+
+# UPDATE - Union find is more suitable for problems where provided input is
+# adjacency list / edge list. Otherwise, we'll need to convert the grid so
+# we can perform union find, which itself is a dfs operation... So don't
+# use union find if the input is grid!
