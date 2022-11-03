2c55ef4fcaa3c07db5ef6c1fff88b0eee7f173d3
KangerDrew <kangdrew95@gmail.com>
2022-11-02 18:08:53 -0700

    added iterative bfs solution

--- studyPlanEasy/day9 - Graph/floodFill.py
+++ studyPlanEasy/day9 - Graph/floodFill.py
@@ -1,3 +1,4 @@
+import collections
 # My first attempt:
 
 def floodFill(image, sr, sc, color):
@@ -20,4 +21,27 @@ def floodFill(image, sr, sc, color):
         return None
 
     dfs(sr, sc)
+    return image
+
+
+# Also solved it using BFS (faster in python because python sucks at recursion):
+def floodFillBFS(image, sr, sc, color):
+    starting = image[sr][sc]
+    queue = collections.deque([(sr, sc)])
+
+    while queue:
+        r, c = queue.popleft()
+
+        if image[r][c] == color:
+            continue
+
+        if image[r][c] == starting:
+
+            image[r][c] = color
+            directions = [(r - 1, c), (r + 1, c), (r, c + 1), (r, c - 1)]
+
+            for new_r, new_c in directions:
+                if 0 <= new_r < len(image) and 0 <= new_c < len(image[0]):
+                    queue.append((new_r, new_c))
+
     return image
