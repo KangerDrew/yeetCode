49fc913c9dcf27e7eebeaa503eabef2f7f1495d7
KangerDrew <kangdrew95@gmail.com>
2022-11-03 16:37:14 -0700

    problem to solve later, added description to help

---
+++ studyPlanEasy/day8 - Binary Search Tree/...lowestCommonAncestorOfBST.py
@@ -0,0 +1,4 @@
+# Given two values, traverse down the BST. ONLY traverse down if both values are
+# either greater than, or less than the current value. Once we reach a point
+# where we can no longer say that both nodes are either to the left or right, we've
+# reached the lowest common ancestor of those two values!
