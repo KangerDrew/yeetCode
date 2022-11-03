fe1f5f628990dffef8cb549771992803a5ff8f3a
KangerDrew <kangdrew95@gmail.com>
2022-11-02 17:57:50 -0700

    recursive solution with memoization

--- studyPlanEasy/day10 - Dynamic Programming/climbingStairs.py
+++ studyPlanEasy/day10 - Dynamic Programming/climbingStairs.py
@@ -1,1 +1,19 @@
 # Going to try to solve this problem recursively...
+
+
+def climbStairs(n):
+
+    memo = {}
+
+    def climbRecur(n):
+        if n in memo:
+            return memo[n]
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        memo[n] = climbRecur(n - 1) + climbRecur(n - 2)
+        return memo[n]
+
+    return climbRecur(n)
