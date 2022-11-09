8893ecefe3bf99216e82aaa5c5d042be550a2b14
KangerDrew <kangdrew95@gmail.com>
2022-11-08 17:14:31 -0800

    described bulls and cows game

---
+++ studyPlanEasy/day13 - Hashmap/bullsAndCows.py
@@ -0,0 +1,3 @@
+# paper and pencil game, where you guess the "secret" value and return how many
+# integers were guessed correctly AND in the same position (Bulls) and how many
+# were in the secret value but are not in correct position (Cows)
