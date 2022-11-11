1f997a6f8f289a1855193aa0f8c747839026b666
KangerDrew <kangdrew95@gmail.com>
2022-11-10 22:18:46 -0800

    solving this problem from memory

--- studyPlanEasy/day12 - Sliding Window & Two Pointer/findAllAnagramsInString.py
+++ studyPlanEasy/day12 - Sliding Window & Two Pointer/findAllAnagramsInString.py
@@ -1,1 +1,17 @@
-# New two pointer problem
+import collections
+# New two pointer problem. We initialize a dictionary that keeps track of the letters
+# in each substring. We will increment the left and right pointers on the main string s,
+# removing and adding the letters using those pointers and checking to see if the
+# current substring is an anagram of the input p:
+
+
+def findAnagrams(s, p):
+    # If the length of s is shorter than p, then it's impossible for
+    # s to contain any anagram of p in the first place:
+    if len(p) > len(s):
+        return []
+
+    # Define a dictionary that contains the number of letters in the p.
+    # We can simply use Counter to do this:
+    p_dict = collections.Counter(p)
+
