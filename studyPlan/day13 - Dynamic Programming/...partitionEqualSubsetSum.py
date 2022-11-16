4685e6a9af7f347e044b08e070d00dc5137009ae
KangerDrew <kangdrew95@gmail.com>
2022-11-16 13:38:02 -0800

    problem setup

---
+++ studyPlan/day13 - Dynamic Programming/...partitionEqualSubsetSum.py
@@ -0,0 +1,6 @@
+# When we calculate the total sum, we need to ensure that we can  create a
+# sub-array that can reach half the total sum. This means that the remaining
+# value(s) will form another sub-array who's sum will be half the total sum!
+
+# If total sum is odd value, and our values only consist of integers, we can't
+# get sub-array who's sum is half the total sum!
