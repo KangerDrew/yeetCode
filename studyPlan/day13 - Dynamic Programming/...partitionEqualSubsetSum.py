3d7368e19aee1d2227b2de2464cd0c013ce7ef4d
KangerDrew <kangdrew95@gmail.com>
2022-11-16 21:29:46 -0800

    O(n * Target) solution added

--- studyPlan/day13 - Dynamic Programming/...partitionEqualSubsetSum.py
+++ studyPlan/day13 - Dynamic Programming/...partitionEqualSubsetSum.py
@@ -4,3 +4,40 @@
 
 # If total sum is odd value, and our values only consist of integers, we can't
 # get sub-array who's sum is half the total sum!
+
+
+def partitionEqualSum(nums):
+    total = sum(nums)
+
+    # If the total is non-even, it is impossible to create two sub-arrays
+    # of same values...
+    if total % 2 != 0:
+        return False
+
+    target = total // 2
+    # Initialize a set that'll keep track of all possible sum values:
+    all_sums = set()
+
+    for n in nums:
+
+        new_all_sums = set()
+        new_all_sums.add(n)
+
+        for s in all_sums:
+            if s == target:
+                return True
+            elif s + n == target:
+                return True
+            elif s + n > target:
+                new_all_sums.add(s)
+                continue
+
+            new_all_sums.add(s + n)
+            new_all_sums.add(s)
+
+        all_sums = new_all_sums
+
+    return True if target in all_sums else False
+
+
+print(partitionEqualSum([1, 5, 11, 5]))
