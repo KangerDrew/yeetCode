e3d7cb43236d5b038a4d7807492e5f36e1a98bcc
KangerDrew <kangdrew95@gmail.com>
2022-11-16 22:52:58 -0800

    interesting recursive solution found online

--- studyPlan/day13 - Dynamic Programming/...partitionEqualSubsetSum.py
+++ studyPlan/day13 - Dynamic Programming/...partitionEqualSubsetSum.py
@@ -60,3 +60,29 @@ def partitionEqualSum(nums):
 # test = [0, 1, 2, 3, 4]
 # print(test[0:2] + test[5:-1])
 
+
+# Recursive solution (not mine)
+def partitionRecur(nums):
+
+    @lru_cache(maxsize=None)
+    def dfs(nums: Tuple[int], n: int, subset_sum: int) -> bool:
+        # Base cases
+        if subset_sum == 0:
+            return True
+        if n == 0 or subset_sum < 0:
+            return False
+        result = (dfs(nums, n - 1, subset_sum - nums[n - 1])
+                  or dfs(nums, n - 1, subset_sum))
+        return result
+
+    # find sum of array elements
+    total_sum = sum(nums)
+
+    # if total_sum is odd, it cannot be partitioned into equal sum subsets
+    if total_sum % 2 != 0:
+        return False
+
+    subset_sum = total_sum // 2
+    n = len(nums)
+    return dfs(tuple(nums), n - 1, subset_sum)
+
