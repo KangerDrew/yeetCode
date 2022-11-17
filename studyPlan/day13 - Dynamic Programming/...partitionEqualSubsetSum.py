9cd7de6c6b6a1846c91fa0bbb8287ca5873e7cfe
KangerDrew <kangdrew95@gmail.com>
2022-11-16 21:50:00 -0800

    explanation added

--- studyPlan/day13 - Dynamic Programming/...partitionEqualSubsetSum.py
+++ studyPlan/day13 - Dynamic Programming/...partitionEqualSubsetSum.py
@@ -20,23 +20,38 @@ def partitionEqualSum(nums):
 
     for n in nums:
 
+        # Create a new set that'll replace the current all_sums set:
         new_all_sums = set()
+
+        # Check if the value itself is the sum. If so, return True:
+        if n == target:
+            return True
+
+        # Add the current n value to the new_all_sums:
         new_all_sums.add(n)
 
+        # Loop through every possible sum values we've gotten so far:
         for s in all_sums:
-            if s == target:
+
+            # If the combination of the s and n is the target, return True:
+            if s + n == target:
                 return True
-            elif s + n == target:
-                return True
+            # If the combination exceeds target, we don't need to add it to
+            # the set (optimization). Just add the s and skip to next loop:
             elif s + n > target:
                 new_all_sums.add(s)
                 continue
 
+            # If neither occurred above, just add the combination:
             new_all_sums.add(s + n)
+            # Remember to ALSO add the s into the new_all_sums (need to add
+            # it back to all_sums):
             new_all_sums.add(s)
 
+        # Set all_sums to new_all_sums:
         all_sums = new_all_sums
 
+    # Check if the target is in all_sums after the loop above:
     return True if target in all_sums else False
 
 
