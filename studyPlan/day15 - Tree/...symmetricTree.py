386759845a99d81dbd499247e3ec3d73c4186aa1
KangerDrew <kangdrew95@gmail.com>
2022-11-21 21:53:48 -0800

    hint added

---
+++ studyPlan/day15 - Tree/...symmetricTree.py
@@ -0,0 +1,3 @@
+# HINT: Think about how you could traverse the given tree, and anoter copy
+# that's exactly the same. Then, how can we traverse those two trees simultaneously
+# to check that the original tree is symmetric?
