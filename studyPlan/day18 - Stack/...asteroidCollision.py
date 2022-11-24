411af7c95d357a2fe1091f356302a1dcad08b5c4
KangerDrew <kangdrew95@gmail.com>
2022-11-23 20:36:56 -0800

    problem approach written down

---
+++ studyPlan/day18 - Stack/...asteroidCollision.py
@@ -0,0 +1,7 @@
+# Use stack data structure to store incoming asteroids.
+
+# HINT: Collision will only occur, if during the for loop, we encounter (incoming asteroid)
+# a negative asteroid and our previous asteroid value was positive!!
+# Furthermore, if the incoming asteroid is positive and recent asteroid was negative, they
+# will not collide since they would have already be moving away from one another!
+
