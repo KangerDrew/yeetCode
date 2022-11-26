13ef797b97be485d6036d2c64ba8bfdc394c1a5e
KangerDrew <kangdrew95@gmail.com>
2022-11-25 17:09:07 -0800

    more comments

--- studyPlan/day20 - Brute Force & Backtracking/...combinationSum.py
+++ studyPlan/day20 - Brute Force & Backtracking/...combinationSum.py
@@ -6,5 +6,14 @@
 # WRONG: [[2, 2, 3], [2, 3, 2], [3, 2, 2], [7]]
 # CORRECT: [[2, 2, 3], [7]]
 
-# To prevent combinations with identical permutations from being recorded, we traverse down
-# the decision tree as...
+# To prevent combinations with identical permutations from being recorded, we use a specific
+# backtracking method. At the beginning, we mark the first element with an index in the candidate
+# array to be used in our possible combination.
+# Then, we ask ourselves, do we continue to incorporate this same candidate in assembling our
+# combination? If we do include it, we increment the total value by that element, add that element
+# to the possible combination, and continue down the decision tree.
+# If we decide not to use the value, we increment the index (signifying we're no longer using any
+# elements before that index), and continue down THAT path instead.
+
+
+
