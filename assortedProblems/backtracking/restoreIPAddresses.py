8aab9611e089cff99b4fdf62dec5835ba40a8379
KangerDrew <kangdrew95@gmail.com>
2022-12-02 18:49:26 -0800

    edge case added

--- assortedProblems/backtracking/restoreIPAddresses.py
+++ assortedProblems/backtracking/restoreIPAddresses.py
@@ -1,2 +1,11 @@
 # Hint: If the input is greater than 12 characters in length, it is impossible
 # to create a valid IP address from it!
+
+
+def restore(s):
+
+    res = []
+
+    # Edge case as we discussed:
+    if len(s) > 12:
+        return res
