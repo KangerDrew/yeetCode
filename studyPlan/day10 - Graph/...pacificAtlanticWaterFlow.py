c3ce427b627aaad2e89c963649e3bcafe7307964
KangerDrew <kangdrew95@gmail.com>
2022-12-05 14:07:22 -0800

    tackling graph problem. remember list cannot be used in hashset/hashmaps

--- studyPlan/day10 - Graph/...pacificAtlanticWaterFlow.py
+++ studyPlan/day10 - Graph/...pacificAtlanticWaterFlow.py
@@ -6,3 +6,25 @@
 # After the traversals are done, we check to see which nodes can BOTH reach the
 # Pacific AND Atlantic Ocean by looping through the sets to check which nodes
 # are present in both sets.
+
+
+def waterFlow(heights):
+
+    row, col = len(heights), len(heights[0])
+    # Pacific Ocean is top and left, Atlantic Ocean is right and bottom:
+    set_pacific, set_atlantic = set(), set()
+
+    # Write a dfs traversal algorithm, that checks which nodes will be able
+    # to reach the current node we've entered. This means, checking neighboring
+    # nodes to see if their height value is greater than or equal to the current
+    # node... Make sure to also pass down the correct set depending on which
+    # ocean you begin your traversal from:
+    def dfs(node, cur_height, ocean_set):
+
+        if node in ocean_set:
+            return
+
+        ocean_set.add(node)
+
+
+
