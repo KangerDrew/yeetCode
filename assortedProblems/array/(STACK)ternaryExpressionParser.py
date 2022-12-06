03ae852cdc0f3c3ec3bb17b01a3ebf88520b8652
KangerDrew <kangdrew95@gmail.com>
2022-12-06 13:34:22 -0800

    premium stack problem

---
+++ assortedProblems/array/(STACK)ternaryExpressionParser.py
@@ -0,0 +1,9 @@
+# Statement: Given a string expression representing arbitrarily nested ternary expressions,
+# evaluate the expression, and return the result of it.
+
+# You can always assume that the given expression is valid and only contains digits,
+# '?', ':', 'T', and 'F' where 'T' is true and 'F' is false. All the numbers in the
+# expression are one-digit numbers (i.e., in the range [0, 9]).
+
+# The conditional expressions group right-to-left (as usual in most languages),
+# the result of the expression will always evaluate to either a digit, 'T' or 'F'.
