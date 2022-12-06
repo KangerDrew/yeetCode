1d3670d5e66388eabcd759549fd28bb0725fe4a0
KangerDrew <kangdrew95@gmail.com>
2022-12-06 14:10:21 -0800

    valid solution added

--- assortedProblems/array/(STACK)ternaryExpressionParser.py
+++ assortedProblems/array/(STACK)ternaryExpressionParser.py
@@ -15,3 +15,39 @@
 # digit/bool(TRUE), followed by ":", and finally a digit/bool(FALSE). We then process the
 # ternary operator (the current entry should tell us if we need to choose the TRUE digit/bool
 # or FALSE one...
+
+
+def parseTernary(expression):
+
+    stack = []
+
+    for s in expression:
+
+        # If the last recent entry in stack was "?", that means our current
+        # s should be a boolean T or F:
+        if stack and stack[-1] == "?":
+
+            # Remove "?"
+            stack.pop()
+            # Get the ternary expression's truth portion:
+            t_portion = stack.pop()
+            # Remove ":"
+            stack.pop()
+            # Get the ternary expression's false portion:
+            f_portion = stack.pop()
+
+            # Append t_portion or f_portion, depending on what our s is currently:
+            if s == "T":
+                stack.append(t_portion)
+            else:
+                stack.append(f_portion)
+
+        # If we haven't reached a processable ternary expression, just keep appending
+        # to the stack:
+        else:
+            stack.append(s)
+
+    # Assuming the ternary expression was valid, there should only be one digit/bool left
+    # in the stack. Return it:
+    return stack[0]
+
