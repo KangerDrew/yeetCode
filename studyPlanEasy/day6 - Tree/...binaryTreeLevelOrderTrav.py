0a64aa13cac462f7977aeee2fa51ca788df5ad2c
Drew Kang <85123383+KangerDrew@users.noreply.github.com>
2023-12-14 11:16:51 -0800

    working solution from memory. should review this topic further...

--- studyPlanEasy/day6 - Tree/...binaryTreeLevelOrderTrav.py
+++ studyPlanEasy/day6 - Tree/...binaryTreeLevelOrderTrav.py
@@ -1,3 +1,33 @@
+from collections import deque
+
 # You can use iterative BFS solution to solve this problem, but there is
 # a way to solve this using recursive "DFS" solution. You just need to
 # keep track of which level you are entering at each recursion stack...
+
+
+def levelOrderTraversal(root):
+
+    if not root:
+        return []
+
+    level_order_res = []
+    queue = deque([root])
+
+    while queue:
+
+        current_level = []
+
+        for n in range(len(queue)):
+
+            current_node = queue.popleft()
+
+            if current_node:
+                current_level.append(current_node.val)
+                queue.append(current_node.left)
+                queue.append(current_node.right)
+
+        if current_level:
+            level_order_res.append(current_level)
+
+    return level_order_res
+
